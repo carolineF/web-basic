40a5b66463ce47f7865397b92d9eb67a1f0b43a9
carolineF <1138483832@qq.com>
2015-08-13 16:50:29 +0800

    add topic type file

---
+++ config/topic-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    FILL_BLACK: "fillBlack",
+    SINGLE_CHOICE: "singleChoice",
+    MULTIPLE_CHOICE: 'multipleChoice',
+    TRUE_FALSE: 'trueFalse',
+    SHORT_ANSWER: 'shortAnswer'
+};
